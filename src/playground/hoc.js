24c78dadeaa64f72524f6a34aa7e27a43f30594d
adojem <adojemcovix@gmail.com>
2018-08-21 05:47:42 +0900

    The Higher Order Component

---
+++ src/playground/hoc.js
@@ -0,0 +1,44 @@
+// Reuse code
+// Render hijacking
+// Prop manipulation
+// Abstract state
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Info = (props) => (
+   <div>
+      <h1>Info</h1>
+      <p>The info is: {props.info}</p>
+   </div>
+);
+
+const withAdminWarning = (WrappedComponent) => {
+   return (props) => (
+      <div>
+         {props.isAdmin && <p>This is private info. don't share!</p>}
+         <WrapperComponent {...props} />
+      </div>
+   );
+};
+
+const requireAuthentication = (WrappedComponent) => {
+   return (props) => (
+      <div>
+         {props.isAuthenticated ? (
+            <WrapperComponent {...props} />
+         ) : (
+            <p>Please login to view the info</p>
+         )}
+      </div>
+   );
+};
+
+const AdminInfo = withAdminWarning(Info);
+const AuthInfo = requireAuthentication(Info);
+
+// ReactDOM.render(<AdminInfo isAdmin={true} info="This is the detail" />, document.getElementById('app'));
+ReactDOM.render(
+   <AuthInfo isAuthenticated={false} info="There are the detail" />,
+   document.getElementById('app')
+);
