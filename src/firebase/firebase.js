e0e755b5b28c93804c795750ed5d41a5ad07c7d4
adojem <adojemcovix@gmail.com>
2018-09-27 15:47:51 +0900

    Fetching Data from Firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -5,33 +5,47 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
-database
-   .ref()
-   .set({
-      name: 'Andrew Mead',
-      age: 26,
-      stressLevel: 6,
-      job: {
-         title: 'Software developer',
-         company: 'Google',
-      },
-      location: {
-         city: 'Philadelphia',
-         country: 'United States',
-      },
-   })
-   .then(() => {
-      console.log('Data is saved.');
-   })
-   .catch((e) => {
-      console.log('This failed.', e);
-   });
-
-database.ref().update({
-   stressLevel: 9,
-   'job/company': 'Amazon',
-   'location/city': 'Seattle',
+database.ref().on('value', (snapshot) => {
+   const val = snapshot.val();
+   console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`);
 });
+
+// database
+//    .ref('location/city')
+//    .once('value')
+//    .then((snapshot) => {
+//       const val = snapshot.val();
+//       console.log(val);
+//    })
+//    .catch(err => console.log('Error fetching data', err));
+
+// database
+//    .ref()
+//    .set({
+//       name: 'Andrew Mead',
+//       age: 26,
+//       stressLevel: 6,
+//       job: {
+//          title: 'Software developer',
+//          company: 'Google',
+//       },
+//       location: {
+//          city: 'Philadelphia',
+//          country: 'United States',
+//       },
+//    })
+//    .then(() => {
+//       console.log('Data is saved.');
+//    })
+//    .catch((e) => {
+//       console.log('This failed.', e);
+//    });
+
+// database.ref().update({
+//    stressLevel: 9,
+//    'job/company': 'Amazon',
+//    'location/city': 'Seattle',
+// });
 
 // database
 //    .ref()
