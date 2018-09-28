cc5dc03548877c3cebc7ba5a1c8190b89f0d03fb
adojem <adojemcovix@gmail.com>
2018-09-28 16:19:25 +0900

    Array Data in Firebase: Part 1

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -5,10 +5,51 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
-database.ref().on('value', (snapshot) => {
-   const val = snapshot.val();
-   console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`);
+database.ref('expenses').push({
+   description: 'Rent',
+   note: '',
+   amount: 109500,
+   createdAt: 976123498763,
 });
+
+database.ref('expenses').push({
+   description: 'Phone bill',
+   note: '',
+   amount: 5900,
+   createdAt: 976123498763,
+});
+
+database.ref('expenses').push({
+   description: 'Food',
+   note: '',
+   amount: 1200,
+   createdAt: 976123498763,
+});
+
+// database.ref('notes').push({
+//    title: 'React Native, Angular, Python',
+//    body: 'Course Topics',
+// });
+
+// const notes = [
+//    {
+//       id: '12',
+//       title: 'First note!',
+//       body: 'This is my note',
+//    },
+//    {
+//       id: '761ase',
+//       title: 'Another note!',
+//       body: 'This is my note',
+//    },
+// ];
+
+// database.ref('notes').set(notes);
+
+// database.ref().on('value', (snapshot) => {
+//    const val = snapshot.val();
+//    console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`);
+// });
 
 // database
 //    .ref('location/city')
