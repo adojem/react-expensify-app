3f749e9c23b70869c3a17851a7f19e90729e4eec
adojem <adojemcovix@gmail.com>
2018-11-22 12:51:21 +0900

    The Auth Reducer

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,6 +2,7 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import expenseReducer from '../reducers/expenses';
 import filterReducer from '../reducers/filters';
+import authReducer from '../reducers/auth';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
@@ -10,9 +11,9 @@ export default () => {
       combineReducers({
          expenses: expenseReducer,
          filters: filterReducer,
+         auth: authReducer,
       }),
       composeEnhancers(applyMiddleware(thunk)),
-      // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
    );
 
    return store;
